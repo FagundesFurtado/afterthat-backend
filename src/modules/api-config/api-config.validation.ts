2feb4d88496c786c7c7395f23958d2ea97aec26c
Cristiano Fagundes <fagundescristianof@gmail.com>
2020-09-09 23:16:38 -0300

    fix: validation

--- src/modules/api-config/api-config.validation.ts
+++ src/modules/api-config/api-config.validation.ts
@@ -1,12 +1,8 @@
 import * as Joi from '@hapi/joi';
 
 export const validationSchema = Joi.object({
-    PORT: Joi.number().required(),
-    DB_USER: Joi.string().required(),
-    DB_PASSWORD: Joi.string().required(),
-    DB_ROOT_PASSWORD: Joi.string(),
-    NODE_ENV: Joi.string()
-        .valid('development', 'production', 'test', 'provision')
-        .required(),
+  PORT: Joi.number().required(),
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test', 'provision')
+    .required(),
 });
-
